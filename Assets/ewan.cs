d48b2aa19296fa43c12dcf56ac025935cfee85f7
brixcel <152989958+brixcel@users.noreply.github.com>
2026-02-10 21:16:29 +0800

    try game

---
+++ Assets/ewan.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ewan : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // ewan ko nalang tlga
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
